a5ec6d5f417a2e4dc6f4dc89e04d91cf4518c714
Maksim Stroganov <furyaxe@yandex.ru>
2022-04-24 12:09:39 +0300

    add lesson 04, 05

---
+++ CPPtm/04/task1.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <list>
+#include <deque>
+
+using namespace std;
+
+template< template<typename, typename> class A, typename B >
+void insert_sorted(A<B, allocator <B>> &v, B n) {
+    auto it = lower_bound(v.begin(), v.end(), n);
+    A<B, allocator <B>> t(1, n);
+    copy(t.begin(), t.end(), inserter(v, it));
+}
+
+int main() {
+    // vector int
+    vector<int> v;
+    v.resize(10);
+    generate(v.begin(), v.end(), []() { return rand() % 100; });
+    sort(v.begin(), v.end());
+    copy(v.begin(), v.end(), ostream_iterator<int>(cout, " "));
+    cout << endl;
+    insert_sorted<vector, int>(v, 55);
+    copy(v.begin(), v.end(), ostream_iterator<int>(cout, " "));
+    cout << endl;
+    // list string
+    const char* s[5] = { "a1", "b2", "c3", "d4", "e5" };
+    list<string> str(s, s + 5);
+    insert_sorted<list, string>(str, "b4");
+    copy(str.begin(), str.end(), ostream_iterator<string>(cout, " "));
+    cout << endl;
+    // deque char
+    char c[5] = { 'b', 'c', 'd', 'e', 'f'};
+    deque<char> dq(c, c + 5);
+    insert_sorted<deque, char>(dq, 'a');
+    copy(dq.begin(), dq.end(), ostream_iterator<char>(cout, " "));
+
+    return 0;
+}
---
+++ CPPtm/05/task2.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <string>
+#include <map>
+#include <algorithm>
+
+using namespace std;
+
+void ltrim(string& str) {
+    while (str[0] == ' ')
+        str.erase(0, 1);
+}
+
+pair<int, string> inserter(const string::iterator& b, const string::iterator& e) {
+    string temp = "";
+    int n = 0;
+    copy(b, e, back_inserter(temp));
+    ltrim(temp);
+    n = count(temp.begin(), temp.end(), ' ') + 1;
+    return { n, temp };
+}
+
+int main() {
+    multimap<int, string> sentences; //контейнер
+    string words;                   //строка целиком до Enter
+    getline(cin, words);
+    string::iterator it = words.begin();
+    string::iterator it1 = words.end();
+    while (it != words.end()) {
+        it1 = find_if(it, it1, [](char ch) { return (ch == '.' || ch == '?' || ch == '!'); });
+        if (it1 != words.end()) { 
+            sentences.insert(inserter(it, it1));
+            it = ++it1;           //смещаем вправо на 1, чтоб перескочить разделитель
+            it1 = words.end();
+        }
+        else {                       //если в конце words нет точки и т.п.
+            sentences.insert(inserter(it, it1));
+            it = it1;
+        }
+    }
+    
+    for_each(sentences.begin(), sentences.end(), [](pair<int, string> p) 
+                        { cout << p.first << " " << p.second << "\n"; });
+    
+    return 0;
+}
