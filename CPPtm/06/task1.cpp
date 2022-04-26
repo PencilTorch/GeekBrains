08518537ba1adb2bad54bec14d6dce8436682825
PencilTorch <borisb342@gmail.com>
2022-04-26 20:39:16 +0300

    add dirr 6 and lesson 6

---
+++ CPPtm/06/task1.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <chrono>
+#include <thread>
+#include <mutex>
+using namespace std;
+
+mutex m;
+
+void pcout() {
+	m.lock();
+	cout << "start pcout in thread id " << this_thread::get_id() << endl;
+	this_thread::sleep_for(1s);
+	cout << "start pcout in thread id " << this_thread::get_id() << endl;
+	m.unlock();
+}
+
+int  main() {
+	thread th1(pcout);
+	thread th2(pcout);
+	thread th3(pcout);
+	th1.join();
+	th2.join();
+	th3.join();
+
+	return 0;
+}
