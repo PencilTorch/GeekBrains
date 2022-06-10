b38726df7d42392566b90a4877033cb35d404920
PencilTorch <furyaxe@yandex.ru>
2022-06-10 13:25:22 +0300

    add 8 lesson in DesignPatternsGOF

---
+++ DesignPatternsGOF/08/Prototype.cpp
@@ -0,0 +1,62 @@
+﻿#include <iostream>
+#define HP_COEF 10
+#define ATK_COEF 2
+#define DEF_COEF 2
+
+class Monster {
+public:
+    Monster(const std::string& type, int hp = 0, int atk = 0, int def = 0) : _type(type), _hp(hp), _atk(atk), _def(def) {}
+    virtual ~Monster() {}
+
+    void changeHp(int change) { _hp += change; }
+    void changeAtk(int change) { _atk += change; }
+    void changeDef(int change) { _def += change; }
+
+    int getHp() const { return _hp; }
+    int getAtk() const { return _atk; }
+    int getDef() const { return _def; }
+    void printCharacters() const {
+        std::cout << _type << ": " << _hp << ", " << _atk << ", " << _def << std::endl;
+    }
+
+    virtual void setLvl(int lvl) = 0;
+    virtual Monster* clone() const = 0;
+private:
+    int _hp;
+    int _atk;
+    int _def;
+    std::string _type;
+};
+
+class MonstersLevel : public Monster {
+public:
+    MonstersLevel(const std::string& type, int hp = 0, int atk = 0, int def = 0) : Monster(type, hp, atk, def) {};
+    void setLvl(int lvl) override {
+        if (lvl > 0 && lvl <= 10) {
+            changeHp(lvl * HP_COEF);
+            changeAtk(lvl * ATK_COEF);
+            changeDef(lvl * DEF_COEF);
+        }
+    }
+    Monster* clone() const override {
+        return new MonstersLevel(*this);
+    }
+};
+
+int main() {
+    Monster* goblin_basic = new MonstersLevel("goblin", 10, 1, 1);
+    Monster* goblin_1lvl = goblin_basic->clone();
+    goblin_1lvl->setLvl(1);
+    Monster* goblin_2lvl = goblin_basic->clone();
+    goblin_2lvl->setLvl(2);
+
+    goblin_basic->printCharacters();
+    goblin_1lvl->printCharacters();
+    goblin_2lvl->printCharacters();
+
+    delete goblin_2lvl;
+    delete goblin_1lvl;
+    delete goblin_basic;
+
+    return 0;
+}
---
+++ DesignPatternsGOF/08/State.cpp
@@ -0,0 +1,121 @@
+﻿#include <iostream>
+using namespace std;
+
+class StateContext;
+class HaveCoin;
+class NoHaveCoin;
+class SoldCoffee;
+class NoCoffee;
+
+class State {
+    string _message;
+public:
+    State(const string& message) : _message(message) {
+        cout << _message << endl;
+    }
+    virtual ~State() {}
+    virtual void returnCoin(StateContext*) {};
+    virtual void buttonBuyPressed(StateContext*) {};
+    virtual void noMoreCoffee(StateContext*) {};
+    virtual void coffeeAvailable(StateContext*) {};
+    virtual void dropeCoin(StateContext*) {};
+    string getMessage() const { return _message; }
+};
+//===============================================================
+class StateContext {
+private:
+    int _countCoffee;
+    State* state;
+public:
+    StateContext(State* state) : state(state) {}
+    ~StateContext() { delete state; }
+
+    void ReturnCoin() {
+        cout << "Return the coin ..." << endl;
+        state->returnCoin(this);
+    }
+    void ButtonBuyPressed() {
+        cout << "The buy button is pressed ..." << endl;
+        _countCoffee--;
+        state->buttonBuyPressed(this);
+        if (_countCoffee > 0) {
+            cout << "Coffee is available ..." << endl;
+            state->coffeeAvailable(this);
+        }
+        else {
+            cout << "Sorry, no more coffee ..." << endl;
+            state->noMoreCoffee(this);
+        }
+    }
+    void DropeCoin() {
+        cout << "Drope the coin ..." << endl;
+        state->dropeCoin(this);
+    }
+
+    void SetState(State* s) {
+        cout << "Chaging state from " << state->getMessage() << " to " << s->getMessage() << "..." << endl;
+        delete state;
+        state = s;
+    }
+    State* GetState() const { return state; }
+    string getMessage() const { return state->getMessage(); }
+    void setCountCoffee(int countCoffee) { _countCoffee = countCoffee; }
+};
+//===============================================================
+class HaveCoin : public State {
+public:
+    HaveCoin() : State("Got a coin") {}
+    void returnCoin(StateContext* state) override;
+    void buttonBuyPressed(StateContext* state) override;
+};
+class NoHaveCoin : public State {
+public:
+    NoHaveCoin() : State("Give me a coin") {}
+    void dropeCoin(StateContext* state) override;
+};
+class SoldCoffee : public State {
+public:
+    SoldCoffee() : State("Coffee bought") {}
+    void noMoreCoffee(StateContext*) override;
+    void coffeeAvailable(StateContext*) override;
+};
+class NoCoffee : public State {
+public:
+    NoCoffee() : State("Don't have coffee") {}
+    void noCoffee(StateContext* state);
+};
+//===============================================================
+void HaveCoin::returnCoin(StateContext* state) {
+    state->SetState(new NoHaveCoin);
+}
+void HaveCoin::buttonBuyPressed(StateContext* state) {
+    state->SetState(new SoldCoffee);
+}
+void NoHaveCoin::dropeCoin(StateContext* state) {
+    state->SetState(new HaveCoin);
+}
+void SoldCoffee::coffeeAvailable(StateContext* state) {
+        state->SetState(new NoHaveCoin);
+}
+void SoldCoffee::noMoreCoffee(StateContext* state) {
+    state->SetState(new NoCoffee);
+}
+void NoCoffee::noCoffee(StateContext* state) {
+    cout << state->getMessage() << endl;
+}
+
+
+int main() {
+    StateContext* pStateContext = new (nothrow) StateContext(new NoHaveCoin());
+    pStateContext->setCountCoffee(2);
+    if (pStateContext != nullptr) {
+        pStateContext->DropeCoin();
+        pStateContext->ButtonBuyPressed();
+        cout << "\n=================\n\n";
+        pStateContext->DropeCoin();
+        pStateContext->ButtonBuyPressed();
+        delete pStateContext;
+    }
+
+    return 0;
+}
