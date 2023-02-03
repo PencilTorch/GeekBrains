e7da8a10fe908ff757eebd5e019325ed00473973
PencilTorch <furyaxe@yandex.ru>
2023-02-03 14:57:45 +0300

    dz6 push for next step

---
+++ Qt/06/QtLanguage_en.ts
@@ -0,0 +1,113 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+<context>
+    <name>FileDialog</name>
+    <message>
+        <location filename="filedialog.cpp" line="7"/>
+        <source>Открыть только для чтения</source>
+        <translation>Open read-only</translation>
+    </message>
+    <message>
+        <location filename="filedialog.cpp" line="9"/>
+        <source>Текстовый файл (*.txt)</source>
+        <translation>Text file (*.txt)</translation>
+    </message>
+    <message>
+        <location filename="filedialog.cpp" line="9"/>
+        <source>Все файлы (*)</source>
+        <translation>All files (*)</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.ui" line="14"/>
+        <source>Текстовый редактор 4.0</source>
+        <translation>Text editor 4.0</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="23"/>
+        <location filename="mainwindow.ui" line="87"/>
+        <source>Открыть</source>
+        <translation>Open</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="30"/>
+        <location filename="mainwindow.ui" line="102"/>
+        <source>Сохранить</source>
+        <translation>Save</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="52"/>
+        <source>Файл</source>
+        <translation>File</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="62"/>
+        <source>Справка</source>
+        <translation>Help</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="66"/>
+        <source>Язык</source>
+        <translation>language</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="90"/>
+        <source>Ctrl+O</source>
+        <translation>Ctrl+O</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="105"/>
+        <source>Ctrl+S</source>
+        <translation>Ctrl+S</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="117"/>
+        <source>Выход</source>
+        <translation>Exit</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="120"/>
+        <source>Ctrl+Q</source>
+        <translation>Ctrl+Q</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="132"/>
+        <source>О программе</source>
+        <translation>About this programm</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="137"/>
+        <source>Русский</source>
+        <translation>Russian</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="142"/>
+        <source>Английский</source>
+        <translation>English</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="38"/>
+        <source>Сохранить файл</source>
+        <translation>Save file</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="38"/>
+        <source>Текстовый файл (*.txt)</source>
+        <translation>Text file (*.txt)</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="60"/>
+        <source>Справка о программе</source>
+        <translation>Help about this programm</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="60"/>
+        <source>Программа является простым тестовым редактором.
+Добавлена опция &quot;Только чтение&quot; и динамическое переключение языка.</source>
+        <translation>Программа является простым текстовым редактором. Добавлена опция только чтение и динамический перевод.</translation>
+    </message>
+</context>
+</TS>
---
+++ Qt/06/QtLanguage_ru.ts
@@ -0,0 +1,192 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ru_RU">
+<context>
+    <name>FileDialog</name>
+    <message>
+        <source>Open read-only</source>
+        <translation type="vanished">Открыть только для чтения</translation>
+    </message>
+    <message>
+        <source>Text file (*.txt)</source>
+        <translation type="vanished">Текстовый файл (*.txt)</translation>
+    </message>
+    <message>
+        <source>All files (*)</source>
+        <translation type="vanished">Все файлы (*)</translation>
+    </message>
+    <message>
+        <location filename="filedialog.cpp" line="7"/>
+        <source>Открыть только для чтения</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="filedialog.cpp" line="9"/>
+        <source>Текстовый файл (*.txt)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="filedialog.cpp" line="9"/>
+        <source>Все файлы (*)</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <source>Text editor 4.0</source>
+        <oldsource>Text editor</oldsource>
+        <translation type="vanished">Текстовый редактор 4.0</translation>
+    </message>
+    <message>
+        <source>Open</source>
+        <translation type="vanished">Открыть</translation>
+    </message>
+    <message>
+        <source>Save</source>
+        <translation type="vanished">Сохранить</translation>
+    </message>
+    <message>
+        <source>File</source>
+        <translation type="vanished">Файл</translation>
+    </message>
+    <message>
+        <source>Help</source>
+        <translation type="vanished">Справка</translation>
+    </message>
+    <message>
+        <source>Language</source>
+        <translation type="vanished">Язык</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="14"/>
+        <source>Текстовый редактор 4.0</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="23"/>
+        <location filename="mainwindow.ui" line="87"/>
+        <source>Открыть</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="30"/>
+        <location filename="mainwindow.ui" line="102"/>
+        <source>Сохранить</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="52"/>
+        <source>Файл</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="62"/>
+        <source>Справка</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="66"/>
+        <source>Язык</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="90"/>
+        <source>Ctrl+O</source>
+        <translation>Ctrl+O</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="105"/>
+        <source>Ctrl+S</source>
+        <translation>Ctrl+S</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="117"/>
+        <source>Выход</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="132"/>
+        <source>О программе</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="137"/>
+        <source>Русский</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="142"/>
+        <source>Английский</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Exit</source>
+        <translation type="vanished">Выход</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="120"/>
+        <source>Ctrl+Q</source>
+        <translation>Ctrl+Q</translation>
+    </message>
+    <message>
+        <source>About the program</source>
+        <translation type="vanished">О программе</translation>
+    </message>
+    <message>
+        <source>Russian</source>
+        <translation type="vanished">Русский</translation>
+    </message>
+    <message>
+        <source>English</source>
+        <translation type="vanished">Английский</translation>
+    </message>
+    <message>
+        <source>Open text file</source>
+        <translation type="vanished">Открыть текстовый файл</translation>
+    </message>
+    <message>
+        <source>Text file (*.txt)</source>
+        <translation type="vanished">Текстовый файл (*.txt)</translation>
+    </message>
+    <message>
+        <source>Save file</source>
+        <translation type="vanished">Сохранить файл</translation>
+    </message>
+    <message>
+        <source>Help about the program</source>
+        <translation type="vanished">Справка о программе</translation>
+    </message>
+    <message>
+        <source>The program is a simple text editor.
+        Added an option &quot;Only read&quot; and dynamic language switching.</source>
+        <translation type="vanished">Программа является простым тестовым редактором.
+Добавлена опция &quot;Только чтение&quot; и динамическое переключение языка.</translation>
+    </message>
+    <message>
+        <source>The program is a simple text editor.</source>
+        <translation type="vanished">Программа является простым тестовым редактором.</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="38"/>
+        <source>Сохранить файл</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="38"/>
+        <source>Текстовый файл (*.txt)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="60"/>
+        <source>Справка о программе</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="60"/>
+        <source>Программа является простым тестовым редактором.
+Добавлена опция &quot;Только чтение&quot; и динамическое переключение языка.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
